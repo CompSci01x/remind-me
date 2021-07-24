0f231a9dedcf1244b538c4b4f6d8f5f84e995926
CompSci01x <42660883+CompSci01x@users.noreply.github.com>
2021-07-24 15:46:40 -0700

    Initial Commit

---
+++ Remind Me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Remind Me
+//
+//  Created by Spruce Tree on 7/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
