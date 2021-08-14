6ca53b8fb51ac6adcab3832f60392325ac22ffe4
CompSci01x <42660883+CompSci01x@users.noreply.github.com>
2021-08-14 08:43:08 -0700

    Reminders List, Add, Edit, and added app icon image

--- Remind Me/AppDelegate.swift
+++ Remind Me/AppDelegate.swift
@@ -15,6 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound],
+                                                                completionHandler: { (granted, error) in
+        })
         return true
     }
 
